e4e939d9721fb1a790a1ec8a4201dff28e45bce8
cjvanlissa <c.j.vanlissa@uu.nl>
2021-12-13 19:27:38 +0100

    first analysis

--- prepare_data.R
+++ prepare_data.R
@@ -9,32 +9,15 @@ library(tidySEM)
 dat <- readxl::excel_sheets("SR_SSRI_FearLearning_ForestPlot_data_20211018_Caspar.xlsx")
 dat <- lapply(dat[-1], function(x){
   tmp <- as.data.frame(readxl::read_xlsx(path = "SR_SSRI_FearLearning_ForestPlot_data_20211018_Caspar.xlsx", sheet = x))
+  tmp[["Remarks"]] <- NULL
   names(tmp) <- tolower(names(tmp))
-  tmp[["remarks"]] <- NULL
+  numers <- tolower(c("Mean experimental group", "SD experimental group",
+              "Number of animals in intervention group", "Mean control group",
+              "SD control group", "Number of animals in control group"))
+  cats <- names(tmp)[!names(tmp) %in% numers]
+  tmp[numers] <- lapply(tmp[numers], as.numeric)
   tmp$Process = x
   tmp
 })
 dat <- tidySEM:::bind_list(dat)
 open_data(dat)
-#   Information regarding this data file for the SR fear learning & SSRIs
-#
-# This data file is organised by fear learning process. This means that every fear learning process has its own Excel sheet.
-#
-# Within these sheets you find the same layout. The different columns are explained below:
-#
-#   Study name
-# Article code
-# Type of SSRI
-# Frequency
-# Disease induction
-# Species
-# Type of test
-# Sensitivity analysis
-#
-#
-#
-# Analyses we want to perform:
-#
-#   1. Separate meta-analyses of the five fear learning processes that have enough data (acq learn to cue, acq learn to ctx, acq retr to cue, acq retr to ctx, ext learn to cue)
-# 2. Subgroup analyses of the five factors (SSRI, frequency, disease induction, species, type of test) per fear learning process
-# 3. Check for multi collinearity
